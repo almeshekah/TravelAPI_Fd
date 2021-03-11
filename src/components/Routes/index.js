9de03c1ea0d8bb3d582757ff516583c7bf0724f2
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-03-11 14:46:12 +0300

    fix the Route

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { useSelector } from 'react-redux';
 //Components
-import Home from "../Home";
-import UserSignin from "../UserSignin";
-import UserSignup from "../UserSignup";
-import AirlineSignup from "../AirlineSignup";
-import AirlineSignin from "../AirlineSignin";
-import FlightList from "../FlightList";
-import AirlineList from "../AirlineList";
-import FlightAdd from "../FlightAdd";
-import AirlineDetail from "../AirlineDetail";
-import Userprofile from "../Userprofile";
-import Flights from "../Flights";
+import Home from '../Home';
+import UserSignin from '../UserSignin';
+import UserSignup from '../UserSignup';
+import AirlineSignup from '../AirlineSignup';
+import AirlineSignin from '../AirlineSignin';
+import FlightList from '../FlightList';
+import AirlineList from '../AirlineList';
+import FlightAdd from '../FlightAdd';
+import AirlineDetail from '../AirlineDetail';
 import Userprofile from '../Userprofile';
+import Flights from '../Flights';
 
 const Routes = () => {
-  const flights = useSelector((state) => state.flightReducer.flights);
-  const airlines = useSelector((state) => state.airlineReducer.airlines);
+	const flights = useSelector((state) => state.flightReducer.flights);
+	const airlines = useSelector((state) => state.airlineReducer.airlines);
 
-  return (
-    <Switch>
-
-    	<Route exact path="/user/profile">
+	return (
+		<Switch>
+			<Route exact path="/user/profile">
 				<Userprofile />
 			</Route>
-      <Route exact path="/usersignup">
-        <UserSignup />
-      </Route>
-      <Route exact path="/usersignin">
-        <UserSignin />
-      </Route>
-      <Route path="/airlinesignup">
-        <AirlineSignup />
-      </Route>
-      <Route path="/airlinesignin">
-        <AirlineSignin />
-      </Route>
+			<Route exact path="/usersignup">
+				<UserSignup />
+			</Route>
+			<Route exact path="/usersignin">
+				<UserSignin />
+			</Route>
+			<Route path="/airlinesignup">
+				<AirlineSignup />
+			</Route>
+			<Route path="/airlinesignin">
+				<AirlineSignin />
+			</Route>
 
-      <Route
-        path={[
-          "/airlines/:airlineId/flights/new",
-          "/airlines/:airlineId/flights/:flightId/update",
-         <FlightAdd />
-      </Route>
-    
-      <Route path="/flightsearch">
-        <Flights />
-      </Route>
+			<Route
+				path={[
+					'/airlines/:airlineId/flights/new',
+					'/airlines/:airlineId/flights/:flightId/update',
+				]}
+			>
+				<FlightAdd />
+			</Route>
 
-      <Route path="/airlines/:airlineSlug">
-        <AirlineDetail flights={flights} />
-      </Route>
-      <Route path="/flights">
-        <FlightList flights={flights} />
-      </Route>
-      <Route path="/airlines">
-        <AirlineList airlines={airlines} />
-      </Route>
+			<Route path="/flightsearch">
+				<Flights />
+			</Route>
+
+			<Route path="/airlines/:airlineSlug">
+				<AirlineDetail flights={flights} />
+			</Route>
+			<Route path="/flights">
+				<FlightList flights={flights} />
+			</Route>
+			<Route path="/airlines">
+				<AirlineList airlines={airlines} />
+			</Route>
 			<Route path="/">
 				<Home />
 			</Route>
 		</Switch>
 	);
-
 };
 
 export default Routes;
