067818dfcdb22bd9f945b97f82189c88c5efd2fb
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-03-14 11:16:47 +0300

    edited create flight

--- src/components/FlightAdd/index.js
+++ src/components/FlightAdd/index.js
@@ -6,7 +6,11 @@ import { register } from "../../serviceWorker";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import Loading from "../Loading";
 import { createFlight, updateFlight } from "../../store/actions/flightActions";
+
+import AirportSelect from "../FlightSearch/AirportSelect";
+//styling
 import {
   FormStyled,
   LabelStyled,
@@ -28,7 +32,9 @@ const FlightAdd = () => {
   );
 
   const airline = useSelector((state) => state.authReducer.user);
-
+  const { destinations, destinationLoading } = useSelector(
+    (state) => state.destinationReducer
+  );
   const [flight, setFlight] = useState(
     foundFlight
       ? foundFlight
@@ -39,129 +45,168 @@ const FlightAdd = () => {
           departureDate: "",
           departureTime: "",
           arrivalTime: "",
-          departureAirport: "",
-          arrivalAirport: "",
+          originId: "",
+          destinationId: "",
           economyPrice: "",
           businessPrice: "",
         }
   );
 
-  console.log(foundFlight);
+  const [options, setOptions] = useState({
+    destinationId: null,
+    originId: null,
+  });
+
+  // console.log("checking update ", foundFlight);
+
+  if (destinationLoading) return <Loading />;
+
+  const departureOptions = destinations.map((destination) => ({
+    value: destination.id,
+    label: `${destination.city} (${destination.code} - ${destination.airport})`,
+    name: "originId",
+  }));
+  const arrivalOptions = departureOptions.map((option) => ({
+    ...option,
+    name: "destinationId",
+  }));
+
+  const handleOptions = (selectedOption) => {
+    setOptions({ ...options, [selectedOption.name]: selectedOption });
+  };
 
   const handleChange = (event) => {
     setFlight({ ...flight, [event.target.name]: event.target.value });
   };
+  {
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (foundFlight) {
+        dispatch(updateFlight(flight, airline.id));
+        console.log(flight);
+      } else {
+        dispatch(
+          createFlight(
+            {
+              ...flight,
+              originId: options.originId.value,
+              destinationId: options.destinationId.value,
+            },
+            airline.id
+          )
+        );
+      }
+      history.replace("/airlines");
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (foundFlight) {
-      dispatch(updateFlight(flight, airline.id));
-    } else dispatch(createFlight(flight, airline.id));
-    history.replace("/airlines");
-  };
-  return (
-    <>
-      <Helmet>
-        <title>Add Flight</title>
-      </Helmet>
-      <FormStyled>
-        <form onSubmit={handleSubmit}>
-          <FieldSetStyled>
-            <LegendStyled>
-              <h2>
-                {foundFlight ? "Update " : "New "}
-                Flight
-              </h2>
+    return (
+      <>
+        <Helmet>
+          <title>Add Flight</title>
+        </Helmet>
+        <FormStyled>
+          <form onSubmit={handleSubmit}>
+            <FieldSetStyled>
+              <LegendStyled>
+                <h2>
+                  {foundFlight ? "Update " : "New "}
+                  Flight
+                </h2>
 
-              <LabelStyled>
-                Name:
-                <InputFieldStyled
-                  type="text"
-                  name="name"
-                  value={flight.name}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Name:
+                  <InputFieldStyled
+                    type="text"
+                    name="name"
+                    value={flight.name}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Departure Date:
-                <InputFieldStyled
-                  type="text"
-                  name="departureDate"
-                  value={flight.departureDate}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
-              <LabelStyled>
-                Departure Time:
-                <InputFieldStyled
-                  type="text"
-                  name="departureTime"
-                  value={flight.departureTime}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Departure Date:
+                  <InputFieldStyled
+                    type="text"
+                    name="departureDate"
+                    value={flight.departureDate}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
+                <LabelStyled>
+                  Departure Time:
+                  <InputFieldStyled
+                    type="text"
+                    name="departureTime"
+                    value={flight.departureTime}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Arrival Time:
-                <InputFieldStyled
-                  type="text"
-                  name="arrivalTime"
-                  value={flight.arrivalTime}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Arrival Time:
+                  <InputFieldStyled
+                    type="text"
+                    name="arrivalTime"
+                    value={flight.arrivalTime}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Departure Airport:
-                <InputFieldStyled
-                  type="text"
-                  name="departureAirport"
-                  value={flight.departureAirport}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Departure Airport:
+                  <AirportSelect
+                    name="origin"
+                    options={options}
+                    handleOptions={handleOptions}
+                    _options={departureOptions}
+                    set="originId"
+                    // value={flight.origin}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Arrival Airport:
-                <InputFieldStyled
-                  type="text"
-                  name="arrivalAirport"
-                  value={flight.arrivalAirport}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Arrival Airport:
+                  <AirportSelect
+                    name="destination"
+                    options={options}
+                    handleOptions={handleOptions}
+                    _options={arrivalOptions}
+                    set="destinationId"
+                    // value={flight.destination}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Price:
-                <InputFieldStyled
-                  type="text"
-                  name="economyPrice"
-                  value={flight.economyPrice}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Price:
+                  <InputFieldStyled
+                    type="text"
+                    name="economyPrice"
+                    value={flight.economyPrice}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
 
-              <LabelStyled>
-                Business Price:
-                <InputFieldStyled
-                  type="text"
-                  name="businessPrice"
-                  value={flight.businessPrice}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+                <LabelStyled>
+                  Business Price:
+                  <InputFieldStyled
+                    type="text"
+                    name="businessPrice"
+                    value={flight.businessPrice}
+                    onChange={handleChange}
+                  />
+                </LabelStyled>
 
-              <FormAddButtonStyled onSubmit={handleSubmit}>
-                {foundFlight ? "Update " : "New "}
-                Flight
-              </FormAddButtonStyled>
-            </LegendStyled>
-          </FieldSetStyled>
-        </form>
-      </FormStyled>
-    </>
-  );
+                <FormAddButtonStyled onSubmit={handleSubmit}>
+                  {foundFlight ? "Update " : "New "}
+                  Flight
+                </FormAddButtonStyled>
+              </LegendStyled>
+            </FieldSetStyled>
+          </form>
+        </FormStyled>
+      </>
+    );
+  }
 };
 
 export default FlightAdd;
