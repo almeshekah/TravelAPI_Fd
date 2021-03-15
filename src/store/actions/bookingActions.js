7330ff53bdcb1b59884ebfd37ff4bd54ed4cde17
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-15 17:20:30 +0300

    booking actions and reducer

--- src/store/actions/bookingActions.js
+++ src/store/actions/bookingActions.js
@@ -1,28 +1,56 @@
-import { CREATE_BOOKING, SET_PASSANGERS } from "./types";
+import { CREATE_BOOKING, SET_DETAILS, SET_FLIGHTS } from "./types";
 import instance from "./instance";
 
-export const bookingCreate = (flightId, history, roundtrip) => async (
-  dispatch
-) => {
+export const bookingCreate = (
+  passengers,
+  bookingReducer,
+  user,
+  history
+) => async (dispatch) => {
   try {
-    // const res = await instance.post(`/booking`, newBooking);
+    const { travelClassId } = bookingReducer;
+    const chosenFlights = bookingReducer.flights;
+    const flights = [chosenFlights.departing];
+    if (chosenFlights.returning) flights.push(chosenFlights.returning);
+    console.log({
+      passengers,
+      flights,
+      travelClassId,
+      userId: user ? user.id : null,
+    });
+    const res = await instance.post(`/booking`, {
+      passengers,
+      flights,
+      travelClassId,
+      userId: user ? user.id : null,
+    });
     await dispatch({
       type: CREATE_BOOKING,
-      payload: { flightId },
+      payload: res.data,
     });
-    history.push(
-      history.location.pathname === "/flightsearch" && roundtrip
-        ? "/returnFlights"
-        : "/booking"
-    );
+    history.push("/");
   } catch (error) {
     console.log(error);
   }
 };
 
-export const passangersNumber = (passangers) => {
+export const passangersDetails = (passengersCount, travelClassId) => {
   return {
-    type: SET_PASSANGERS,
-    payload: { passangers },
+    type: SET_DETAILS,
+    payload: { passengersCount, travelClassId },
   };
+};
+
+export const chosenFlights = (flightId, history, roundtrip, type) => async (
+  dispatch
+) => {
+  await dispatch({
+    type: SET_FLIGHTS,
+    payload: { [type]: flightId },
+  });
+  history.push(
+    history.location.pathname === "/flightsearch" && roundtrip
+      ? "/returnFlights"
+      : "/booking"
+  );
 };
