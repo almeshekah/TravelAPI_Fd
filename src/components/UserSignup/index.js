25e3f11cbc9d3033dcb80a2710b01f5a9a90d322
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-03-18 09:00:44 +0300

    fix profileUpdate

--- src/components/UserSignup/index.js
+++ src/components/UserSignup/index.js
@@ -1,168 +1,168 @@
-import React from "react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { register } from "../../serviceWorker";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { signup, updateProfile } from "../../store/actions/authActions";
+import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { register } from '../../serviceWorker';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import { signup, updateProfile } from '../../store/actions/authActions';
 //Styling
 import {
-  FormStyled,
-  LabelStyled,
-  InputFieldStyled,
-  FieldSetStyled,
-  LegendStyled,
-  FormAddButtonStyled,
-} from "../../styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+	FormStyled,
+	LabelStyled,
+	InputFieldStyled,
+	FieldSetStyled,
+	LegendStyled,
+	FormAddButtonStyled,
+} from '../../styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const UserSignup = () => {
-  const eye = <FontAwesomeIcon icon={faEye} />;
-  const profile = useSelector((state) => state.authReducer.profile);
-  const { errors } = useForm();
-  const history = useHistory();
+	const eye = <FontAwesomeIcon icon={faEye} />;
+	const profile = useSelector((state) => state.authReducer.profile);
+	const { errors } = useForm();
+	const history = useHistory();
 
-  const [passwordShown, setPasswordShown] = useState(false);
-  const togglePasswordVisiblity = () => {
-    setPasswordShown(passwordShown ? false : true);
-  };
+	const [passwordShown, setPasswordShown] = useState(false);
+	const togglePasswordVisiblity = () => {
+		setPasswordShown(passwordShown ? false : true);
+	};
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const [user, setUser] = useState(
-    profile ?? {
-      username: "",
-      password: "",
-      email: "",
-      firstName: "",
-      lastName: "",
-      passport: "",
-      picture: "",
-    }
-  );
+	const [user, setUser] = useState(
+		profile ?? {
+			username: '',
+			password: '',
+			email: '',
+			firstName: '',
+			lastName: '',
+			passport: '',
+			picture: '',
+		}
+	);
 
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+	const handleChange = (event) => {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	};
 
-  const handlePicture = (event) => {
-    setUser({ ...user, picture: event.target.files[0] });
-  };
+	const handlePicture = (event) => {
+		setUser({ ...user, picture: event.target.files[0] });
+	};
 
-  const restForm = () => {
-    setUser({
-      username: "",
-      password: "",
-      email: "",
-      firstName: "",
-      lastName: "",
-      passport: "",
-      picture: "",
-    });
-  };
+	const restForm = () => {
+		setUser({
+			username: '',
+			password: '',
+			email: '',
+			firstName: '',
+			lastName: '',
+			passport: '',
+			picture: '',
+		});
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (profile) {
-      dispatch(updateProfile(user));
-      history.replace("/");
-    } else dispatch(signup(user, history));
-    restForm();
-  };
-  return (
-    <>
-      <Helmet>
-        <title>{profile ? "Update" : "Sign Up"} </title>
-      </Helmet>
-      <FormStyled>
-        <form onSubmit={handleSubmit}>
-          <FieldSetStyled>
-            <LegendStyled>
-              <h2>Sign Up</h2>
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (profile) {
+			dispatch(updateProfile(user));
+			history.replace('/');
+		} else dispatch(signup(user, history));
+		restForm();
+	};
+	return (
+		<>
+			<Helmet>
+				<title></title>
+			</Helmet>
+			<FormStyled>
+				<form onSubmit={handleSubmit}>
+					<FieldSetStyled>
+						<LegendStyled>
+							<h2>{profile ? 'Update' : 'Sign Up'}</h2>
 
-              <LabelStyled>
-                First Name :
-                <InputFieldStyled
-                  type="text"
-                  name="firstName"
-                  value={user.firstName}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+							<LabelStyled>
+								First Name :
+								<InputFieldStyled
+									type="text"
+									name="firstName"
+									value={user.firstName}
+									onChange={handleChange}
+								/>
+							</LabelStyled>
 
-              <LabelStyled>
-                Last Name :
-                <InputFieldStyled
-                  type="text"
-                  name="lastName"
-                  value={user.lastName}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
-              <LabelStyled>
-                Passport:
-                <InputFieldStyled
-                  type="text"
-                  name="passport"
-                  value={user.passport}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+							<LabelStyled>
+								Last Name :
+								<InputFieldStyled
+									type="text"
+									name="lastName"
+									value={user.lastName}
+									onChange={handleChange}
+								/>
+							</LabelStyled>
+							<LabelStyled>
+								Passport:
+								<InputFieldStyled
+									type="text"
+									name="passport"
+									value={user.passport}
+									onChange={handleChange}
+								/>
+							</LabelStyled>
 
-              <LabelStyled>
-                Username:
-                <InputFieldStyled
-                  type="text"
-                  name="username"
-                  value={user.username}
-                  onChange={handleChange}
-                />
-              </LabelStyled>
-              {profile ? null : (
-                <LabelStyled>
-                  Password:
-                  <span>
-                    <i onClick={togglePasswordVisiblity}>{eye}</i>
-                  </span>
-                  <InputFieldStyled
-                    type={passwordShown ? "text" : "password"}
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                    ref={register({ required: true, minLength: 8 })}
-                  />
-                  {errors.password && <p>Pass is required!!</p>}
-                </LabelStyled>
-              )}
+							<LabelStyled>
+								Username:
+								<InputFieldStyled
+									type="text"
+									name="username"
+									value={user.username}
+									onChange={handleChange}
+								/>
+							</LabelStyled>
+							{profile ? null : (
+								<LabelStyled>
+									Password:
+									<span>
+										<i onClick={togglePasswordVisiblity}>{eye}</i>
+									</span>
+									<InputFieldStyled
+										type={passwordShown ? 'text' : 'password'}
+										name="password"
+										value={user.password}
+										onChange={handleChange}
+										ref={register({ required: true, minLength: 8 })}
+									/>
+									{errors.password && <p>Pass is required!!</p>}
+								</LabelStyled>
+							)}
 
-              <LabelStyled>
-                Picture:
-                <InputFieldStyled
-                  type="file"
-                  name="picture"
-                  onChange={handlePicture}
-                />
-              </LabelStyled>
-              <LabelStyled>
-                Email:
-                <InputFieldStyled
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                />
-              </LabelStyled>
+							<LabelStyled>
+								Picture:
+								<InputFieldStyled
+									type="file"
+									name="picture"
+									onChange={handlePicture}
+								/>
+							</LabelStyled>
+							<LabelStyled>
+								Email:
+								<InputFieldStyled
+									type="email"
+									name="email"
+									onChange={handleChange}
+								/>
+							</LabelStyled>
 
-              <FormAddButtonStyled onSubmit={handleSubmit}>
-                {profile ? "Update" : "Sign Up"}
-              </FormAddButtonStyled>
-            </LegendStyled>
-          </FieldSetStyled>
-        </form>
-      </FormStyled>
-    </>
-  );
+							<FormAddButtonStyled onSubmit={handleSubmit}>
+								{profile ? 'Update' : 'Sign Up'}
+							</FormAddButtonStyled>
+						</LegendStyled>
+					</FieldSetStyled>
+				</form>
+			</FormStyled>
+		</>
+	);
 };
 
 export default UserSignup;
--- src/components/Userprofile/index.js
+++ src/components/Userprofile/index.js
@@ -1,62 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { DetailWrapper } from "./styles";
-import Loading from "../Loading";
-import { useSelector, useDispatch } from "react-redux";
-import { history } from "../../store/actions/authActions";
-import { ItemWrapper } from "./styles";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { DetailWrapper } from './styles';
+import Loading from '../Loading';
+import { useSelector, useDispatch } from 'react-redux';
+import { history } from '../../store/actions/authActions';
+import { ItemWrapper } from './styles';
 
 const Userprofile = () => {
-  const dispatch = useDispatch();
-  const profile = useSelector((state) => state.authReducer.profile);
-  const loading = useSelector((state) => state.authReducer.loading);
-  if (loading) return <Loading />;
+	const dispatch = useDispatch();
+	const profile = useSelector((state) => state.authReducer.profile);
+	const loading = useSelector((state) => state.authReducer.loading);
+	if (loading) return <Loading />;
 
-  return (
-    <>
-      <ItemWrapper>
-        <h1 style={{ marginBottom: "2%", marginLeft: "2.5%", marginTop: "2%" }}>
-          {profile.lastName}
-        </h1>
-        <img
-          src={profile.picture}
-          alt={profile.firstName}
-          // style={{
-          //   borderRadius: "50%",
-          //   border: "2px solid transparent",
-          //   width: "20%",
-          // }}
-        />
-        <p>
-          my name :{profile.firstName}
-          {profile.lastName}
-        </p>
-        <p>passport:{profile.passport}</p>
-        <p>email:{profile.email}</p>
-      </ItemWrapper>
-      <DetailWrapper>
-        <Link to="/user/signup">
-          <button
-            type="button"
-            className="btn btn-dark "
-            style={{ position: "absolute", left: "1030px", top: "240px" }}
-          >
-            Edit My Profile
-          </button>
-        </Link>
+	return (
+		<>
+			<ItemWrapper>
+				<h1 style={{ marginBottom: '2%', marginLeft: '2.5%', marginTop: '2%' }}>
+					{profile.lastName}
+				</h1>
+				<img
+					src={profile.picture}
+					alt={profile.firstName}
+					// style={{
+					//   borderRadius: "50%",
+					//   border: "2px solid transparent",
+					//   width: "20%",
+					// }}
+				/>
+				<p>
+					my name :{profile.firstName}
+					{profile.lastName}
+				</p>
+				<p>passport:{profile.passport}</p>
+				<p>email:{profile.email}</p>
+			</ItemWrapper>
+			<DetailWrapper>
+				<Link to="/user/profile/edit">
+					<button
+						type="button"
+						className="btn btn-dark "
+						style={{ position: 'absolute', left: '1030px', top: '240px' }}
+					>
+						Edit My Profile
+					</button>
+				</Link>
 
-        <Link onClick={() => dispatch(history())} to="/user/order-history">
-          <button
-            type="button"
-            className="btn btn-dark "
-            style={{ position: "absolute", left: "1030px", top: "350px" }}
-          >
-            My Orders History
-          </button>
-        </Link>
-      </DetailWrapper>
-    </>
-  );
+				<Link onClick={() => dispatch(history())} to="/user/order-history">
+					<button
+						type="button"
+						className="btn btn-dark "
+						style={{ position: 'absolute', left: '1030px', top: '350px' }}
+					>
+						My Orders History
+					</button>
+				</Link>
+			</DetailWrapper>
+		</>
+	);
 };
 
 export default Userprofile;
