ba1a46200200cc3c856d930ba82f7abf7831f3bc
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-03-18 13:03:50 +0300

    fixed-table

--- src/components/FlightList/FlightItem.js
+++ src/components/FlightList/FlightItem.js
@@ -1,38 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { ItemWrapper } from "./styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableRow from "@material-ui/core/TableRow";
 
 import UpdateButton from "../buttons/UpdateButton";
 
 const FlightItem = ({ flight }) => {
+  const stringTime = (time) => {
+    let hrs = Math.floor(time);
+    let mins = Math.round((time % 1) * 60);
+    return `${hrs > 12 ? hrs - 12 : hrs}:${mins > 9 ? mins : `0${mins}`} ${
+      hrs >= 12 ? "PM" : "AM"
+    }`;
+  };
+  const stringDate = (date) => date.split("-").reverse().join("/");
   return (
     <>
       <Helmet>
         <title>Flights</title>
       </Helmet>
-      <Table>
-        <TableRow>
-          <TableCell style={{ width: 100 }}>{flight.name}</TableCell>
-          <TableCell>{flight.departureDate}</TableCell>
-          <TableCell>{flight.departureTime}</TableCell>
-          <TableCell>{flight.arrivalTime}</TableCell>
-          <TableCell>{flight.departureAirport}</TableCell>
-          <TableCell>{flight.arrivalAirport}</TableCell>
-          <TableCell>{flight.economyPrice}</TableCell>
-          <TableCell>{flight.businessPrice}</TableCell>
-          <TableCell>{flight.airlineId}</TableCell>
-          <TableCell>
-            <UpdateButton
-              airlineId={flight.airlineId}
-              flightId={flight.id}
-            ></UpdateButton>
-          </TableCell>
-        </TableRow>
-      </Table>
+      <tr>
+        <td>{flight.name}</td>
+        <td>{stringDate(flight.departureDate)}</td>
+        <td>{stringTime(flight.departureTime)}</td>
+        <td>{stringTime(flight.arrivalTime)}</td>
+        <td>{flight.economyPrice} BD</td>
+        <td>{flight.businessPrice} BD</td>
+        <td>
+          <UpdateButton
+            airlineId={flight.airlineId}
+            flightId={flight.id}
+          ></UpdateButton>
+        </td>
+      </tr>
     </>
   );
 };
